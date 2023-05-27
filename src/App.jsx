008d3ce9258c051562296e22be5e620c4c18bdaf
Pratik katti <90851204+pratt4@users.noreply.github.com>
2023-05-27 15:26:49 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import "./style.css"
+
+function App() {
+  const [newItem,setNewItem] =useState("")
+
+  const[todos,setTodos]=useState([])
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    setTodos(currentTodos=>{
+      return [
+        ...currentTodos,
+        {
+          id:crypto.randomUUID(),title:newItem,completed:false
+        },
+      ]
+    })
+
+    setNewItem("")
+
+    
+  }
+  function toggletodo(id ,completed){
+    setTodos(currentTodos=>{
+      return currentTodos.map(todo=>{
+        if(todo.id===id){
+          return{...todo,completed}
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos=>{
+      return currentTodos.filter(todo=>todo.id!==id)
+    })
+  }
+
+  // console.log(todos)
+
+  return<> <form onSubmit={handleSubmit}className="new-item-form">
+    <div className="form-row">
+      <label htmlFor="item">New Item</label>
+      <input value={newItem} onChange={e=>setNewItem(e.target.value)}type="text" id="item" />
+
+
+    </div>
+
+    <button className="btn">ADD</button>
+  </form>
+
+  <h1 className="header">TODO LIST</h1>
+
+  <ul className="list">
+
+    {todos.length==0 && "No Todos"}
+
+  {todos.map(todo=>{
+    return( <li key={todo.id}>
+      <label  >
+        <input type="checkbox"  checked={todo.completed} onChange={e=>toggletodo(todo.id,e.target.checked)} />
+        {todo.title}
+      </label>
+      <button onClick={()=>deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
+    </li>)
+  })}
+   
+    
+  </ul>
+  </>
+}
+
+export default App;
